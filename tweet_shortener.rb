d05f63e11612104e7387dfe5ea79095cc6883436
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-17 19:41:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -11,19 +11,19 @@ def dictionary
   "and" => "&"}
  end
 
-def word_substituter(tweet)
-  dic = dictionary
-  tweetArray = tweet.split
-  editedTweet = []
-  tweetArray.each do |word|
-    if dic.has_key?(word)== true
-      editedTweet << dic[word]
-    else
-    editedTweet<< word
-    end
-  end
-  editedTweet.join(" ")
-end
+ def word_substituter(tweet)
+   dic = dictionary
+   tweetArray = tweet.split
+   editedTweet = []
+   tweetArray.each do |word|
+     if dic.has_key?(word)== true
+       editedTweet << dic[word]
+     else
+     editedTweet<< word
+     end
+   end
+   editedTweet.join(" ")
+ end
 
 def bulk_tweet_shortener(array_of_tweets)
   array_of_tweets.each do |tweet|
