79405554ccf03b89d702afc4480bc1f12d0a632f
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-17 19:52:27 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -26,7 +26,7 @@ def dictionary
  end
 
  def bulk_tweet_shortener(array_of_tweets)
-   array_of_tweets.each do |tweet|
+   array_of_tweets.map do |tweet|
      puts word_substituter(tweet)
    end
  end
