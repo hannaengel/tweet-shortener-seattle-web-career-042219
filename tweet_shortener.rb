55d4908e447e6c12ec9f1c8eecea4aa3a6e5f1e6
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-17 19:34:43 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,9 +1,25 @@
 def dictionary
   dictionary = {"hello"=>'hi',
-  "to, two, too" => '2',
+  "to" => '2',
+  "two"=> '2',
+  "too" => '2',
   "for, four" => '4',
   'be' => 'b',
   'you' => 'u',
   "at" => "@" ,
   "and" => "&"}
+ end
+
+def word_substituter(tweet)
+  dic = dictionary
+  tweetArray = tweet.split
+  editedTweet = []
+  tweetArray.each do |word|
+    if dic.has_key?(word)== true
+      editedTweet << dic[word]
+    else
+    editedTweet<< word
+    end
+  end
+  editedTweet.join(" ")
 end
